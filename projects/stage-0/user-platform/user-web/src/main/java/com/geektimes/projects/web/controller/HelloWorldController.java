af99147cd10309f446cb2731f6fe4cd87788fe68
liuwanyong <wanyong_liu@163.com>
2021-03-11 10:47:38 +0800

    添加web基本框架元素

---
+++ projects/stage-0/user-platform/user-web/src/main/java/com/geektimes/projects/web/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.geektimes.projects.web.controller;
+
+import org.geektimes.web.mvc.controller.PageController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+public class HelloWorldController implements PageController {
+
+    @GET
+    @Path("/world")
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        return "world.jsp";
+    }
+}
